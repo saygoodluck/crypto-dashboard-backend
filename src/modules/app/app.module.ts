d54636edd330161750f53242eb204896001f7be8
Kostiantyn Havrylenko <kkk@kkk.com>
2025-02-13 02:21:50 +0100

    logging interceptor

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -10,10 +10,10 @@ import { AdminModule } from '../admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { AccountTypeGuard } from '../auth/guards/account-type.guard';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AccountRightsGuard } from '../auth/guards/account-rights.guard';
-import { ThrottlerBehindProxyGuard } from '../auth/guards/rate.guard';
 import { RedisModule } from '../redis/redis.module';
+import { LoggingInterceptor } from '../../utils/logging.interceptor';
 
 @Module({
   imports: [
@@ -38,6 +38,7 @@ import { RedisModule } from '../redis/redis.module';
   ],
   controllers: [AppController],
   providers: [
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
     { provide: APP_GUARD, useClass: AccountTypeGuard },
     { provide: APP_GUARD, useClass: AccountRightsGuard }
   ]
