c93c5fddb638ffe686da389268c46d95956b24bc
Kostiantyn Havrylenko <kkk@kkk.com>
2025-02-21 22:16:06 +0100

    cert ssl

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    cert: fs.readFileSync('/usr/local/share/ssl/cert.pem'),
-    key: fs.readFileSync('/usr/local/share/ssl/privkey.pem')
-  };
-  const app = await NestFactory.create(AppModule, { cors: { origin: '*' }, httpsOptions });
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   const configService = app.get(ConfigService);
   const logger = new Logger(bootstrap.name);
 
